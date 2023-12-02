81a1fb7ebefa51461a56b2435f0b2f55505a4ef6
cosmiccutie <73052717+wolfinwhitevan@users.noreply.github.com>
2023-12-02 21:10:02 +0800

    users blade css

--- resources/views/Users/user_subjects_add.blade.php
+++ resources/views/Users/user_subjects_add.blade.php
@@ -6,6 +6,75 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel CRUD</title>
 </head>
+
+<style>
+    body {
+        font-family: 'HelveticaNeue', sans-serif;
+        background-color: #141414;
+        margin-top: 80px;
+        margin-left: 400px;
+        margin-right: 400px;
+    }
+
+    h1 {
+        color: #e0e0e0;
+    }
+
+    a {
+        text-decoration: none;
+        color: #3498db;
+        transition: color 0.1s ease;
+    }
+
+    a:hover {
+        color: #4CAF50;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 20px;
+    }
+
+    td {
+        border: 1px solid #e0e0e0;
+        padding: 12px;
+        text-align: left;
+        color: #e0e0e0;
+    }
+
+    th {
+        background-color: #e0e0e0;
+        color: #141414;
+        border: 1px solid #141414;
+        padding: 12px;
+        text-align: left;
+    }
+
+    tbody tr:nth-child(even) {
+        background-color: #141414;
+    }
+
+    span {
+        display: block;
+        margin-top: 10px;
+        color: #e0e0e0;
+    }
+
+    .delete-btn {
+        background-color: #e74c3c;
+        color: #fff;
+        border: none;
+        padding: 8px 16px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    .delete-btn:hover {
+        background-color: #c0392b;
+    }
+</style>
+
 <body>
     <h1>Add User Subjects</h1>
     <h3>Name: {{$user->name}}</h3>
--- resources/views/Users/users.blade.php
+++ resources/views/Users/users.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel CRUD</title>
 </head>
+
 <style>
     body {
         font-family: 'HelveticaNeue', sans-serif;
